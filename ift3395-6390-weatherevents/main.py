cacaead16ae2b6386df5c551f217ca464cf8e684
Samuel Aguilar-Lussier <sam.aglussier@gmail.com>
2021-11-02 17:31:42 -0400

    code cleanup

--- ift3395-6390-weatherevents/main.py
+++ ift3395-6390-weatherevents/main.py
@@ -1,17 +1,16 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, scale, normalize, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, scale
 from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-# from tensorflow import keras
-from sklearn.feature_selection import SelectKBest, chi2
+from tensorflow import keras
 from logistic_reg import MyLogClassifier
-from sklearn.svm import SVC, NuSVC
+from sklearn.svm import SVC
 
 
 
@@ -88,150 +87,136 @@ x_train_pca = apply_pca(pca_object, x_train)
 x_val_pca = apply_pca(pca_object, x_val)
 test_pca = apply_pca(pca_object, test)
 
-'''
-#<<<----- My log reg ------>>>
-log_classifier = MyLogClassifier()
-log_classifier.train(x=x_train, t=y_train, epochs=10000, learning_rate=0.99)
-predictions = log_classifier.predict(x_val)
-print('My log reg : \n', classification_report(y_val, predictions, zero_division=0))
-print(confusion_matrix(y_val, predictions))
 
-test_predictions = log_classifier.predict(test)
-test_predictions_df = pd.DataFrame(test_predictions)
-create_submission_csv(test_predictions_df, 'predictions')
+def my_logistic_reg() :
 
-'''
+    log_classifier = MyLogClassifier()
+    log_classifier.train(x=x_train, t=y_train, epochs=10000, learning_rate=0.99)
+    predictions = log_classifier.predict(x_val)
+    print('My log reg : \n', classification_report(y_val, predictions, zero_division=0))
+    print(confusion_matrix(y_val, predictions))
 
-
-'''
-#---gaussian naive bayes predictions---
-
-gauss_nb_classifier = train_gauss_naive_bayes(scale(x_train), y_train, count_train/train_len)
-predictions = gauss_nb_classifier.predict(scale(x_val))
-
-print(classification_report(y_val, predictions, target_names=target_names))
-
-test_predictions_gnb = gauss_nb_classifier.predict(scale(test))
-# test_predictions_df = pd.DataFrame(test_predictions)
-# create_submission_csv(test_predictions_df, 'predictions_val')
-'''
-
-'''
-# ---scikit logistic regression---
+    test_predictions = log_classifier.predict(test)
+    test_predictions_df = pd.DataFrame(test_predictions)
+    create_submission_csv(test_predictions_df, 'predictions')
 
 
-logistic_classifier = LogisticRegression(random_state=8, max_iter=500)
-logistic_classifier.fit(scale(x_train), y_train)
-predictions = logistic_classifier.predict(scale(x_val))
+def gaussian_naive_bayes() :
 
-print(classification_report(y_val, predictions))#, target_names=target_names))
+    gauss_nb_classifier = train_gauss_naive_bayes(scale(x_train), y_train, count_train/train_len)
+    predictions = gauss_nb_classifier.predict(scale(x_val))
+    print(classification_report(y_val, predictions, target_names=target_names))
 
-test_predictions_lc = logistic_classifier.predict(scale(test))
-# test_predictions_df = pd.DataFrame(test_predictions)
-# create_submission_csv(test_predictions_df, 'predictions_')
-# print(confusion_matrix(y_val, predictions))
-'''
-
-'''
-#---decision tree---
-
-decision_tree_classifier = DecisionTreeClassifier(random_state=8)
-decision_tree_classifier.fit(x_train, y_train)
-predictions = decision_tree_classifier.predict(x_val)
-print('Decision Tree :\n', classification_report(y_val, predictions, target_names=target_names))
-test_predictions = decision_tree_classifier.predict(test)
-test_predictions_df = pd.DataFrame(test_predictions)
-create_submission_csv(test_predictions_df, 'predictions')
-'''
-
-# '''
-# ---random forest---
-
-random_forest_classifier = RandomForestClassifier(max_depth=12, random_state=8, n_estimators=200, min_samples_leaf=25)
-random_forest_classifier.fit(scale(x_train), y_train)
-predictions = random_forest_classifier.predict(scale(x_val))
-print('Random Forest : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=1))
-
-test_predictions_rf = random_forest_classifier.predict(scale(test))
-test_predictions_df = pd.DataFrame(test_predictions_rf)
-create_submission_csv(test_predictions_df, 'predictions')
-# '''
-
-'''
-# gradient boosting
-
-# grad_boost_classifier = GradientBoostingClassifier(max_depth=8, random_state=8, n_estimators=300, learning_rate=0.1, min_samples_leaf=5)
-grad_boost_classifier = GradientBoostingClassifier(random_state=1, n_estimators=500, min_samples_leaf=150)
-grad_boost_classifier.fit(x_train, y_train)
-predictions = grad_boost_classifier.predict(x_val)
-print('gradient boosting : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=1))
-
-test_predictions_gb = grad_boost_classifier.predict(test)
-test_predictions_gb = pd.DataFrame(test_predictions_gb)
-create_submission_csv(test_predictions_gb, 'predictions')
+    test_predictions_gnb = gauss_nb_classifier.predict(scale(test))
+    test_predictions_df = pd.DataFrame(test_predictions_gnb)
+    create_submission_csv(test_predictions_df, 'predictions_val')
 
 
+def scikit_logistic_reg():
 
-'''
+    logistic_classifier = LogisticRegression(random_state=8, max_iter=500)
+    logistic_classifier.fit(scale(x_train), y_train)
+    predictions = logistic_classifier.predict(scale(x_val))
+    print(classification_report(y_val, predictions))
 
-'''
-from numpy.random import seed
-seed(1)
-# from tensorflow import set_random_seed
-# set_random_seed(2)
-
-n_features = x_train_pca.shape[1]
-
-dnn = keras.Sequential()
-dnn.add(keras.layers.Dense(128, activation='relu', input_dim=n_features))
-dnn.add(keras.layers.Dense(1024, activation='relu'))
-dnn.add(keras.layers.Dense(2048, activation='relu'))
-# dnn.add(keras.layers.Dense(2048, activation='relu'))
-dnn.add(keras.layers.Dense(1024, activation='relu'))
-dnn.add(keras.layers.Dense(128, activation='relu'))
-dnn.add(keras.layers.Dense(1, activation='sigmoid'))
-
-dnn.summary()
-
-learning_rate = 0.005
-adam_optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-sgd_optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+    test_predictions_lc = logistic_classifier.predict(scale(test))
+    test_predictions_df = pd.DataFrame(test_predictions_lc)
+    create_submission_csv(test_predictions_df, 'predictions')
+    print(confusion_matrix(y_val, predictions))
 
 
-dnn.compile(
-    optimizer=adam_optimizer,
-    loss='categorical_crossentropy',
-    metrics=['Precision', 'Accuracy']
-)
+def decision_tree():
 
-# callback = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.001, patience=15,  restore_best_weights=True, mode='auto')
+    decision_tree_classifier = DecisionTreeClassifier(random_state=8)
+    decision_tree_classifier.fit(x_train, y_train)
+    predictions = decision_tree_classifier.predict(x_val)
+    print('Decision Tree :\n', classification_report(y_val, predictions, target_names=target_names))
 
-dnn.fit(x_train_pca, y_train, epochs=5, batch_size=128)#, callbacks=[callback])
-predictions = dnn.predict(x_val_pca)
-print(np.unique(predictions))
+    test_predictions = decision_tree_classifier.predict(test)
+    test_predictions_df = pd.DataFrame(test_predictions)
+    create_submission_csv(test_predictions_df, 'predictions')
 
-print('Dnn : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=0))
-print(confusion_matrix(y_val, predictions))
 
-test_predictions = dnn.predict(test_pca)
-test_predictions_df = pd.DataFrame(test_predictions)
-create_submission_csv(test_predictions_df, 'predictions')
-'''
+def random_forest():
 
-'''
+    random_forest_classifier = RandomForestClassifier(max_depth=12, random_state=8, n_estimators=100, min_samples_leaf=100)
+    random_forest_classifier.fit(scale(x_train), y_train)
+    predictions = random_forest_classifier.predict(scale(x_val))
+    print('Random Forest : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=1))
 
-# ----- SVM -----
-svm_classifier = SVC(decision_function_shape='ovo')
-svm_classifier.fit(scale(x_train), y_train)
-predictions = svm_classifier.predict(scale(x_val))
-print('SVM : \n', classification_report(y_val, predictions, zero_division=0))
-print(confusion_matrix(y_val, predictions))
+    test_predictions_rf = random_forest_classifier.predict(scale(test))
+    test_predictions_df = pd.DataFrame(test_predictions_rf)
+    create_submission_csv(test_predictions_df, 'predictions')
 
-test_predictions_svm = svm_classifier.predict(scale(test))
-# test_predictions_df = pd.DataFrame(test_predictions)
-# create_submission_csv(test_predictions_df, 'predictions')
 
-'''
+def gradient_boosting() :
+
+    grad_boost_classifier = GradientBoostingClassifier(max_depth=8, random_state=8, n_estimators=300, learning_rate=0.1, min_samples_leaf=5)
+    grad_boost_classifier.fit(x_train, y_train)
+    predictions = grad_boost_classifier.predict(x_val)
+    print('gradient boosting : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=1))
+
+    test_predictions_gb = grad_boost_classifier.predict(test)
+    test_predictions_gb = pd.DataFrame(test_predictions_gb)
+    create_submission_csv(test_predictions_gb, 'predictions')
+
+
+def dnn() :
+    from numpy.random import seed
+    seed(1)
+    # from tensorflow import set_random_seed
+    # set_random_seed(2)
+
+    n_features = x_train_pca.shape[1]
+
+    dnn = keras.Sequential()
+    dnn.add(keras.layers.Dense(128, activation='relu', input_dim=n_features))
+    dnn.add(keras.layers.Dense(1024, activation='relu'))
+    dnn.add(keras.layers.Dense(2048, activation='relu'))
+    # dnn.add(keras.layers.Dense(2048, activation='relu'))
+    dnn.add(keras.layers.Dense(1024, activation='relu'))
+    dnn.add(keras.layers.Dense(128, activation='relu'))
+    dnn.add(keras.layers.Dense(1, activation='sigmoid'))
+
+    dnn.summary()
+
+    learning_rate = 0.005
+    adam_optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    sgd_optimizer = keras.optimizers.SGD(learning_rate=learning_rate)
+
+
+    dnn.compile(
+        optimizer=adam_optimizer,
+        loss='categorical_crossentropy',
+        metrics=['Precision', 'Accuracy']
+    )
+
+    # callback = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.001, patience=15,  restore_best_weights=True, mode='auto')
+
+    dnn.fit(x_train_pca, y_train, epochs=5, batch_size=128)#, callbacks=[callback])
+    predictions = dnn.predict(x_val_pca)
+    print(np.unique(predictions))
+
+    print('Dnn : \n', classification_report(y_val, predictions, target_names=target_names, zero_division=0))
+    print(confusion_matrix(y_val, predictions))
+
+    test_predictions = dnn.predict(test_pca)
+    test_predictions_df = pd.DataFrame(test_predictions)
+    create_submission_csv(test_predictions_df, 'predictions')
+
+
+def svm():
+
+    svm_classifier = SVC(decision_function_shape='ovo')
+    svm_classifier.fit(scale(x_train), y_train)
+    predictions = svm_classifier.predict(scale(x_val))
+    print('SVM : \n', classification_report(y_val, predictions, zero_division=0))
+    print(confusion_matrix(y_val, predictions))
+
+    test_predictions_svm = svm_classifier.predict(scale(test))
+    test_predictions_df = pd.DataFrame(test_predictions_svm)
+    create_submission_csv(test_predictions_df, 'predictions')
+
 
 
 # comb_pred = np.zeros((len(test_predictions_gnb), 3))
