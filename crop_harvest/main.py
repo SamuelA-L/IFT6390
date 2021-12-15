f39058c211abc8793ea0b18bebab1545d59d100c
Samuel Aguilar-Lussier <sam.aglussier@gmail.com>
2021-12-15 14:37:01 -0500

    experiment with frandom forest

--- crop_harvest/main.py
+++ crop_harvest/main.py
@@ -8,9 +8,9 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,
 from sklearn.pipeline import make_pipeline
 import xgboost as xgb
 from sklearn.model_selection import KFold
-from lightgbm import LGBMClassifier
-import keras
-import tensorflow as tf
+# from lightgbm import LGBMClassifier
+# import keras
+# import tensorflow as tf
 
 def create_submission_file(predictions, name='predictions'):
     predictions_df = pd.DataFrame(predictions)
@@ -47,18 +47,33 @@ x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_st
 # # create_submission_file(boost_pca_pipeline.predict(x_test).astype(int))
 
 # #find best hyperparams
-# for i in [None, 50]:
-#     for j in [50, 100, 200]:
+# for i in [None, 5, 25, 50]:
+#     for j in [200, 300, 400]:
 #         for k in [1, 10, 50]:
 #             print('max_dept : ', i, " n_estimators : ", j, " min sample leaf : ", k)
 #             forest = RandomForestClassifier(random_state=1, max_depth=i, n_estimators=j, min_samples_leaf=k)
 #             train_and_eval(forest, x_train, y_train, x_val, y_val)
 
-'''
-forest = RandomForestClassifier(random_state=1, max_depth=None, n_estimators=200, min_samples_leaf=1)
+
+# none 200 1
+# none 300 1
+# none 400 1
+
+forest = RandomForestClassifier(random_state=1, max_depth=10, n_estimators=400, min_samples_leaf=1)
 train_and_eval(forest, x_train, y_train, x_val, y_val)
 create_submission_file(forest.predict(x_test).astype(int))
-'''
+
+
+#
+# kf = KFold(n_splits=5, random_state=1, shuffle=True)
+#
+# for train_index, test_index in kf.split(x):
+#      print("TRAIN:", len(train_index), "TEST:", len(test_index))
+#      x_train_fold, x_val_fold = x[train_index], x[test_index]
+#      y_train_fold, y_val_fold = y[train_index], y[test_index]
+#      rf = RandomForestClassifier(random_state=1, max_depth=None, n_estimators=200, min_samples_leaf=1)
+#      train_and_eval(rf, x_train_fold, y_train_fold, x_val_fold, y_val_fold)
+
 
 # ada_boost = AdaBoostClassifier(random_state=1, n_estimators=250, learning_rate=1.5)
 # train_and_eval(ada_boost, x_train, y_train, x_val, y_val)
@@ -76,9 +91,9 @@ for i in [2, 4, 6, 8, 10]:
 
 # best hyperparams found : 8 max_dept, 200 estimators
 
-xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=250, max_depth=50)
-train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
-create_submission_file(xg_boost.predict(x_test).astype(int))
+# xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=250, max_depth=50)
+# train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
+# create_submission_file(xg_boost.predict(x_test).astype(int))
 
 
 # kf = KFold(n_splits=5, random_state=1, shuffle=True)
