1330ba2e121b8701bb168866befce73c6d0e215a
Samuel Aguilar-Lussier <sam.aglussier@gmail.com>
2021-12-17 09:20:20 -0500

    refactor main

--- crop_harvest/main.py
+++ crop_harvest/main.py
@@ -40,66 +40,57 @@ print('class distribution on training data \n', int(counts[0][0]), ' : ', counts
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=1)
 
 
-# boost = GradientBoostingClassifier(random_state=1)
-# train_and_eval(boost, x_train, y_train, x_val, y_val)
-# # boost_pca_pipeline = make_pca_pipeline(boost)
-# # train_and_eval(boost_pca_pipeline, x_train, y_train, x_val, y_val)
-# # create_submission_file(boost_pca_pipeline.predict(x_test).astype(int))
-
-# #find best hyperparams
-# for i in [None, 50]:
-#     for j in [50, 100, 200]:
-#         for k in [1, 10, 50]:
-#             print('max_dept : ', i, " n_estimators : ", j, " min sample leaf : ", k)
-#             forest = RandomForestClassifier(random_state=1, max_depth=i, n_estimators=j, min_samples_leaf=k)
-#             train_and_eval(forest, x_train, y_train, x_val, y_val)
-
-'''
-forest = RandomForestClassifier(random_state=1, max_depth=None, n_estimators=200, min_samples_leaf=1)
-train_and_eval(forest, x_train, y_train, x_val, y_val)
-create_submission_file(forest.predict(x_test).astype(int))
-'''
-
-# ada_boost = AdaBoostClassifier(random_state=1, n_estimators=250, learning_rate=1.5)
-# train_and_eval(ada_boost, x_train, y_train, x_val, y_val)
-# create_submission_file(ada_boost.predict(x_test).astype(int))
+def find_best_hyperparams_rf() :
+    for i in [None, 50]:
+        for j in [50, 100, 200]:
+            for k in [1, 10, 50]:
+                print('max_dept : ', i, " n_estimators : ", j, " min sample leaf : ", k)
+                forest = RandomForestClassifier(random_state=1, max_depth=i, n_estimators=j, min_samples_leaf=k)
+                train_and_eval(forest, x_train, y_train, x_val, y_val)
 
 
-'''
-# find best hyper params
-for i in [2, 4, 6, 8, 10]:
-    for j in [50, 100, 150, 200]:
-        print('max_dept : ', i , " n_estimators : ", j)
-        xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=j, max_depth=i)
-        train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
-'''
+# best hyperparams found : None max_dept, 200 estimators, 1 min_sample_leaf
+
+def best_rf():
+    forest = RandomForestClassifier(random_state=1, max_depth=None, n_estimators=200, min_samples_leaf=1)
+    train_and_eval(forest, x_train, y_train, x_val, y_val)
+    create_submission_file(forest.predict(x_test).astype(int))
+
+
+def ada_boost():
+    ada_boost = AdaBoostClassifier(random_state=1, n_estimators=250, learning_rate=1.5)
+    train_and_eval(ada_boost, x_train, y_train, x_val, y_val)
+    create_submission_file(ada_boost.predict(x_test).astype(int))
+
+
+def find_best_hyper_params_xgboost() :
+    for i in [2, 4, 6, 8, 10]:
+        for j in [50, 100, 150, 200]:
+            print('max_dept : ', i , " n_estimators : ", j)
+            xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=j, max_depth=i)
+            train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
+
 
 # best hyperparams found : 8 max_dept, 200 estimators
 
-xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=250, max_depth=50)
-train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
-create_submission_file(xg_boost.predict(x_test).astype(int))
+def best_xgboost():
+    xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=250, max_depth=50)
+    train_and_eval(xg_boost, x_train, y_train, x_val, y_val)
+    create_submission_file(xg_boost.predict(x_test).astype(int))
 
 
-# kf = KFold(n_splits=5, random_state=1, shuffle=True)
-#
-# for train_index, test_index in kf.split(x):
-#      print("TRAIN:", len(train_index), "TEST:", len(test_index))
-#      x_train_fold, x_val_fold = x[train_index], x[test_index]
-#      y_train_fold, y_val_fold = y[train_index], y[test_index]
-#      xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=200, max_depth=8)
-#      train_and_eval(xg_boost, x_train_fold, y_train_fold, x_val_fold, y_val_fold)
+def test_xgboost_generalization():
+    kf = KFold(n_splits=5, random_state=1, shuffle=True)
 
-# xgboost with pca
-
-# for i in [2, 4, 6, 8, 10, 25]:
-#     for j in [50, 100, 150, 200, 250]:
-#         print('max_depth : ', i, ' n_estim : ', j)
-#         xgb_pca = make_pca_pipeline(xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=j, max_depth=i))
-#         train_and_eval(xgb_pca, x_train, y_train, x_val, y_val)
-#         # create_submission_file(xgb_pca.predict(x_test).astype(int))
+    for train_index, test_index in kf.split(x):
+         print("TRAIN:", len(train_index), "TEST:", len(test_index))
+         x_train_fold, x_val_fold = x[train_index], x[test_index]
+         y_train_fold, y_val_fold = y[train_index], y[test_index]
+         xg_boost = xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=200, max_depth=8)
+         train_and_eval(xg_boost, x_train_fold, y_train_fold, x_val_fold, y_val_fold)
 
 
-# xgb_pca = make_pca_pipeline(xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=200, max_depth=75))
-# train_and_eval(xgb_pca, x_train, y_train, x_val, y_val)
-# create_submission_file(xgb_pca.predict(x_test).astype(int))
+def test_pca_xgboost():
+    xgb_pca = make_pca_pipeline(xgb.XGBClassifier(random_state=1, eval_metric='logloss', use_label_encoder=False, n_estimators=200, max_depth=75))
+    train_and_eval(xgb_pca, x_train, y_train, x_val, y_val)
+    create_submission_file(xgb_pca.predict(x_test).astype(int))
